4cc7bd3e2e202893057a186b17ee4ed7e6693089
vachg <vachgen@yandex.ru>
2024-11-19 20:39:38 +0300

    HW-Collection

---
+++ src/main/java/collectionsemployee/collectionsEmployee/CollectionsEmployeeApplication.java
@@ -0,0 +1,13 @@
+package collectionsemployee.collectionsEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsEmployeeApplication.class, args);
+	}
+
+}
